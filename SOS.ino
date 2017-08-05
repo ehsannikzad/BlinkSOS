f6b3a758d5cf892d90233f8ae7a86cee313de415
ehsannikzad <30365511+ehsannikzad@users.noreply.github.com>
2017-08-05 11:31:03 +1000

    Add files via upload

---
+++ SOS.ino
@@ -0,0 +1,48 @@
+void setup() {
+  //initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+digitalWrite(LED_BUILTIN, HIGH);
+delay(500);
+digitalWrite(LED_BUILTIN, LOW);
+delay(1000);
+digitalWrite(LED_BUILTIN, HIGH);   
+delay(500);                       
+digitalWrite(LED_BUILTIN, LOW);   
+delay(1000);                      
+digitalWrite(LED_BUILTIN, HIGH);  
+delay(500);                       
+digitalWrite(LED_BUILTIN, LOW);   
+delay(1000);
+
+digitalWrite(LED_BUILTIN, HIGH);
+delay(1000);
+digitalWrite(LED_BUILTIN, LOW);
+delay(500);
+digitalWrite(LED_BUILTIN, HIGH);   
+delay(1000);                       
+digitalWrite(LED_BUILTIN, LOW);   
+delay(500);                      
+digitalWrite(LED_BUILTIN, HIGH);  
+delay(1000);                       
+digitalWrite(LED_BUILTIN, LOW);   
+delay(500);
+
+digitalWrite(LED_BUILTIN, HIGH);
+delay(500);
+digitalWrite(LED_BUILTIN, LOW);
+delay(1000);
+digitalWrite(LED_BUILTIN, HIGH);   
+delay(500);                       
+digitalWrite(LED_BUILTIN, LOW);   
+delay(1000);                      
+digitalWrite(LED_BUILTIN, HIGH);  
+delay(500);                       
+digitalWrite(LED_BUILTIN, LOW);   
+delay(1000);
+}
